bb34769564ef4b35960812ace2db1b521bd7ac72
2GMon <2gmon.t@gmail.com>
2015-03-27 17:00:01 +0900

    リンク貼るスクリプト追加

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+ln -s ~/dotfiles/mlterm ~/.mlterm
+
+ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -s ~/dotfiles/zsh/zsh ~/.zsh
+
+ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/tmux/tmux ~/.tmux
+
+ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/dotfiles/vim/vim ~/.vim
+
+ln -s ~/dotfiles/vimperator/vimperatorrc ~/.vimperatorrc
+ln -s ~/dotfiles/vimperator/vimperator ~/.vimperator
+
+ln -s ~/dotfiles/gem/gemrc ~/.gemrc

ffb381550dc91f49ac0bb921592f75531b90c3b9
2GMon <2gmon.t@gmail.com>
2025-09-10 16:48:46 +0900

    [Neovim] lspの設定変更

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -16,8 +16,3 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 require("mason").setup()
 require("mason-lspconfig").setup()
-require("mason-lspconfig").setup_handlers {
-  function(server_name)
-    require("lspconfig")[server_name].setup {}
-  end,
-}

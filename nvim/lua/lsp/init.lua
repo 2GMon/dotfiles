b64910de5c266d5139a5512023b28c1591216275
2GMon <2gmon.t@gmail.com>
2025-09-10 17:15:06 +0900

    [Neovim] lspのkeymapを変更

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -2,15 +2,12 @@ vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(ctx)
     local set = vim.keymap.set
     set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { buffer = true })
-    set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { buffer = true })
-    set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { buffer = true })
-    set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", { buffer = true })
-    set("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", { buffer = true })
-    set("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { buffer = true })
+    set("n", "grd", "<cmd>lua vim.lsp.buf.definition()<CR>", { buffer = true })
+    set("n", "grD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { buffer = true })
     set("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", { buffer = true })
     set("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", { buffer = true })
-    set("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", { buffer = true })
-    set("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", { buffer = true })
+    set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { buffer = true })
+    set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", { buffer = true })
   end,
 })
 

8f5a0341f11c69345206aff976b0e1f05dd3aeee
2GMon <2gmon.t@gmail.com>
2025-09-11 17:27:18 +0900

    [Neovim] gemini-cliをtoggletermで起動

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -6,25 +6,42 @@ require("toggleterm").setup{
   shade_terminals = true,
   shading_factor = 2,
   start_in_insert = true,
-  insert_mappings = true,
-  persist_size = true,
+  insert_mappings = false,
+  persist_size = false,
   direction = 'float',
   close_on_exit = true,
 }
+local trim_spaces = true
+vim.keymap.set("v", "<space>sl", function()
+    require("toggleterm").send_lines_to_terminal("single_line", trim_spaces, { args = vim.v.count })
+end)
+vim.keymap.set("v", "<space>svl", function()
+    require("toggleterm").send_lines_to_terminal("visual_lines", trim_spaces, { args = vim.v.count })
+end)
+vim.keymap.set("v", "<space>svs", function()
+    require("toggleterm").send_lines_to_terminal("visual_selection", trim_spaces, { args = vim.v.count })
+end)
 
 function _G.set_terminal_keymaps()
   local opts = {buffer = 0}
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'JK', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-w>h', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-w>j', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-w>k', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-w>l', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w><C-w>', [[<C-\><C-n><C-w>]], opts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 local Terminal  = require('toggleterm.terminal').Terminal
+
+-- lazygit の起動
 local lazygit = Terminal:new({
   cmd = "lazygit",
   dir = "git_dir",
-  --hidden = true,
+  count = 99,
   on_open = function(term)
     vim.cmd("startinsert!")
     vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
@@ -39,4 +56,18 @@ function _lazygit_toggle()
   lazygit:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+
+-- gemini-cliの起動
+local gemini = Terminal:new({
+  cmd = "gemini",
+  dir = "git_dir",
+  count = 100,
+  direction="vertical",
+})
+
+function _gemini_toggle()
+  gemini:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>gc", "<cmd>lua _gemini_toggle()<CR>", {noremap = true, silent = true})

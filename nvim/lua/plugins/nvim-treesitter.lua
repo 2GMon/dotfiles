b638bfcc9664b49d8367808cc6472bb3fd1d57d8
2GMon <2gmon.t@gmail.com>
2025-09-11 17:52:52 +0900

    [Neovim] treesitterを絞る

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,15 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = 'all',
+  ensure_installed = {
+    "bash", "c", "cmake", "cpp", "css", "csv", "diff", "dockerfile",
+    "editorconfig", "git_config", "git_rebase", "gitattributes",
+    "gitcommit", "gitignore", "go", "gomod", "gosum", "gowork",
+    "html", "ini", "javascript", "jq", "json", "json5", "jsonc",
+    "lua", "luadoc", "make", "markdown", "markdown_inline",
+    "meson", "nginx", "passwd", "perl", "php", "phpdoc", "printf",
+    "python", "regex", "ruby", "rust", "scala", "scss", "slim",
+    "solidity", "sql", "terraform", "tmux", "toml", "tsv",
+    "typescript", "vim", "vimdoc", "vue", "xml", "yaml"
+  },
   sync_install = false,
   highlight = {
     enable = true,

f54088a3260ca144a12f382b501f290492b403d7
2GMon <2gmon.t@gmail.com>
2025-09-14 01:22:07 +0900

    [Neovim] copilotchat.nvimのキーマップ改善

--- nvim/lua/plugins/copilotchat.lua
+++ nvim/lua/plugins/copilotchat.lua
@@ -1,39 +1,32 @@
 require("CopilotChat").setup({
+  language = "Japanese",
   show_help = "yes",
   prompts = {
     Explain = {
-      prompt = "/COPILOT_EXPLAIN コードを日本語で説明してください",
       mapping = '<leader>ce',
       description = "コードの説明をお願いする",
     },
     Review = {
-      prompt = '/COPILOT_REVIEW コードを日本語でレビューしてください。',
       mapping = '<leader>cr',
       description = "コードのレビューをお願いする",
     },
     Fix = {
-      prompt = "/COPILOT_FIX このコードには問題があります。バグを修正したコードを表示してください。説明は日本語でお願いします。",
       mapping = '<leader>cf',
       description = "コードの修正をお願いする",
     },
     Optimize = {
-      prompt = "/COPILOT_REFACTOR 選択したコードを最適化し、パフォーマンスと可読性を向上させてください。説明は日本語でお願いします。",
       mapping = '<leader>co',
       description = "コードの最適化をお願いする",
     },
     Docs = {
-      prompt = "/COPILOT_GENERATE 選択したコードに関するドキュメントコメントを日本語で生成してください。",
       mapping = '<leader>cd',
       description = "コードのドキュメント作成をお願いする",
     },
     Tests = {
-      prompt = "/COPILOT_TESTS 選択したコードの詳細なユニットテストを書いてください。説明は日本語でお願いします。",
       mapping = '<leader>ct',
       description = "テストコード作成をお願いする",
     },
     Commit = {
-      prompt =
-      '実装差分に対するコミットメッセージを日本語で記述してください。',
       mapping = '<leader>cco',
       description = "コミットメッセージの作成をお願いする",
       selection = require('CopilotChat.select').gitdiff,
@@ -49,23 +42,6 @@ require("CopilotChat").setup({
     },
   },
 })
-
-function CopilotChatBuffer()
-  local input = vim.fn.input("Quick Chat: ")
-  if input ~= "" then
-    require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
-  end
-end
-
--- <leader>ccq (Copilot Chat Quick) で Copilot とチャットする
-vim.api.nvim_set_keymap("n", "<leader>ccq", "<cmd>lua CopilotChatBuffer()<cr>", { noremap = true, silent = true })
-
--- telescope を使ってアクションプロンプトを表示する
-function ShowCopilotChatActionPrompt()
-  local actions = require("CopilotChat.actions")
-  require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
-end
-
--- キーマッピング
--- <leader>ccp (Copilot Chat Prompt の略) でアクションプロンプトを表示する
-vim.api.nvim_set_keymap("n", "<leader>ccp", "<cmd>lua ShowCopilotChatActionPrompt()<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>cct", "<cmd>CopilotChatToggle<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ccp", "<cmd>CopilotChatPrompts<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ccm", "<cmd>CopilotChatModels<cr>", { noremap = true, silent = true })
